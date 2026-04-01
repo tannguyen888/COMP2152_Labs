8fe336bba68913ed20b13d3b5f17a8c9ad5f9a0f
tannguyen888 <triumphdash00@gmai.com>
2026-04-01 13:01:28 -0400

    Remove finished

--- Week1/Week3/Q1.py
+++
@@ -1,7 +0,0 @@
-grades =[85, 92, 78, 95, 88]
-grades.append(90)
-grades.sort()
-print("Sorted grades:", grades)
-print("Highest grade:", grades[-1])
-print("Lowest grade:", grades[0])
-print("Total number of grades:", len(grades))
